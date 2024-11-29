12cefb220883c76b1a4d96579fbbc211aaba1c5e
Andy Hutton <Andyhutton1983@hotmail.com>
2024-11-29 22:21:02 +0000

    fixed a bug in the main.py where clock was not applied in the right place, updated the movement forward and completed chapter 2 for the player

--- main.py
+++ main.py
@@ -5,25 +5,36 @@ from player import Player
 def main():
 # Stuff before the game loop starts
    pygame.init()
-   screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
    clock = pygame.time.Clock()
-   dt = 0
+
+   updatable = pygame.sprite.Group()
+   drawable = pygame.sprite.Group()
+
+   Player.containers = (updatable, drawable)
+
    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+   dt = 0
+   
 # Game loop
    while True:
       for event in pygame.event.get():
          if event.type == pygame.QUIT:
             return
          
-         player.update(dt)
-         
-         screen.fill("black")
-         player.draw(screen)
-         pygame.display.flip()
+      for obj in updatable:
+         obj.update(dt)
+
+      screen.fill("black")
+
+      for obj in drawable:
+         obj.draw(screen)
+
+      pygame.display.flip()
 
          # limit the frame rates to 60 fps.
-         dt = clock.tick(60) / 1000
+      dt = clock.tick(60) / 1000
 
 
 # Stuff after the game loop ends
