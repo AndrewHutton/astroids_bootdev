d3443ccae4ecd24baf8a7e208e018bdb5adc9ab4
Andy Hutton <Andyhutton1983@hotmail.com>
2024-11-30 12:28:34 +0000

    Ch3 L1 complete - added asterorid.py and asteroidfield.py updated main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
 # Stuff before the game loop starts
@@ -10,10 +12,14 @@ def main():
 
    updatable = pygame.sprite.Group()
    drawable = pygame.sprite.Group()
+   asteroids = pygame.sprite.Group()
 
    Player.containers = (updatable, drawable)
-
    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+   Asteroid.containers = (updatable, drawable, asteroids)
+   AsteroidField.containers = (updatable)
+   asteroid_field = AsteroidField()
 
    dt = 0
    
